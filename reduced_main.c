8fc0f04d4776464b6a5ba8f646eaff243d6ee8a8
Ahmed Mohamed <=>
2023-05-24 12:55:23 +0000

    Betty

--- reduced_main.c
+++ reduced_main.c
@@ -187,7 +187,7 @@ size_t handle_not_found(char *argVec, int bol_main, char *message,
 int _strcp(char *a, char *b)
 {
 	int size_b, size = 0;
-	
+
 	while (*a == ' ')
 		a++;
 	size_b = size_of(b, 0);
